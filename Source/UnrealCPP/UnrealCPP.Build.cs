cbf81dad4dc24059c6dbb605190d091c2a07c219
Mustafa Arifoglu <mustafarifoglu@gmail.com>
2025-12-16 20:29:23 +0000

    Debug and Actor Move

--- Source/UnrealCPP/UnrealCPP.Build.cs
+++ Source/UnrealCPP/UnrealCPP.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 
@@ -8,7 +8,7 @@ public class UnrealCPP : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- Source/UnrealCPPEditor.Target.cs
+++ Source/UnrealCPPEditor.Target.cs
@@ -1,15 +1,15 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class UnrealCPPEditorTarget : TargetRules
 {
-	public UnrealCPPEditorTarget( TargetInfo Target) : base(Target)
+	public UnrealCPPEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
-		ExtraModuleNames.Add("UnrealCPP");
+
+		ExtraModuleNames.AddRange( new string[] { "UnrealCPP" } );
 	}
 }
